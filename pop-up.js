ac727a471f26bf40dcc141599735e24e4f769392
Jasmin Dzogovic <jasminxhogovic@gmail.com>
2023-07-31 11:50:24 -0400

    updated to more modern js techniques

--- pop-up.js
+++ pop-up.js
@@ -1,7 +1,12 @@
-function openForm() {
-    document.getElementById("myform").style.display = "block";
-  }
-  
-  function closeForm() {
-    document.getElementById("myform").style.display = "none";
-  }
+const form = document.getElementById("myform");
+const formBtn = document.querySelector(".login-btn");
+
+formBtn.addEventListener("click", () => {
+  form.style.display = "block";
+});
+
+const closeBtn = document.querySelector(".cancel");
+
+closeBtn.addEventListener("click", ()=> {
+  form.style.display = "none";
+})
