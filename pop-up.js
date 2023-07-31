b2e5337e910e9f33f0d1f0987cee32dc890cb589
Jasmin Dzogovic <jasminxhogovic@gmail.com>
2023-07-31 11:56:17 -0400

    updated input fields of form

--- pop-up.js
+++ pop-up.js
@@ -1,12 +1,29 @@
 const form = document.getElementById("myform");
 const formBtn = document.querySelector(".login-btn");
+const closeBtn = document.querySelector(".cancel");
+const emailInput = document.querySelector(".form__email");
+const passwordInput = document.querySelector(".form__password");
 
 formBtn.addEventListener("click", () => {
   form.style.display = "block";
 });
 
-const closeBtn = document.querySelector(".cancel");
-
-closeBtn.addEventListener("click", ()=> {
+closeBtn.addEventListener("click", () => {
   form.style.display = "none";
-})
+});
+
+emailInput.addEventListener("focus", () => {
+  emailInput.placeholder = "";
+});
+
+emailInput.addEventListener("blur", () => {
+  emailInput.placeholder = "Please enter your email";
+});
+
+passwordInput.addEventListener("focus", () => {
+  passwordInput.placeholder = "";
+});
+
+passwordInput.addEventListener("blur", () => {
+  passwordInput.placeholder = "Enter Password";
+});
